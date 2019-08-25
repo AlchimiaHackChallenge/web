3f0d0907ee0ef172971946e8dbe6a307d5aa5af4
Juancho <linanjm90@gmail.com>
2019-08-24 20:00:21 -0500

    add the carousel in the home component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,33 @@
 <template>
-  <h1>Home</h1>
+  <v-carousel
+    cycle="true"
+    >
+    <v-carousel-item
+      v-for="(item,i) in items"
+      :key="i"
+      :src="item.src"
+      reverse-transition="fade-transition"
+      transition="fade-transition"
+      ></v-carousel-item>
+  </v-carousel>
 </template>
 <script>
-  export default {
+export default {
+    data: () => ({
+        items: [
+            { src: 'http://www.italika.mx/WebVisorArchivosITK/Archivo.aspx?Tipo=3&Archivo=WebPortalMexicoITK/img/Slide/Mexico/slide-Modena.jpg'},
+            { src: 'http://www.italika.mx/WebVisorArchivosITK/Archivo.aspx?Tipo=3&Archivo=WebPortalMexicoITK/img/Slide/Mexico/refacciones.jpg'},
+            { src: 'http://www.italika.mx/WebVisorArchivosITK/Archivo.aspx?Tipo=3&Archivo=WebPortalMexicoITK/img/Slide/Mexico/slide-Fiera.jpg'},
+            { src: 'http://www.italika.mx/WebVisorArchivosITK/Archivo.aspx?Tipo=3&Archivo=WebPortalMexicoITK/img/Slide/Mexico/Beneficios.jpg'},
+            { src: 'http://www.italika.mx/WebVisorArchivosITK/Archivo.aspx?Tipo=3&Archivo=WebPortalMexicoITK/img/Slide/Mexico/slide-renta-de-locales.jpg'},
+            { src: 'http://www.italika.mx/WebVisorArchivosITK/Archivo.aspx?Tipo=3&Archivo=WebPortalMexicoITK/img/Slide/Mexico/SLIDE-emplacado.jpg'},
+            { src: 'http://www.italika.mx/WebVisorArchivosITK/Archivo.aspx?Tipo=3&Archivo=WebPortalMexicoITK/img/Slide/Mexico/italikaracing.jpg'},
+            { src: 'http://www.italika.mx/WebVisorArchivosITK/Archivo.aspx?Tipo=3&Archivo=WebPortalMexicoITK/img/Slide/Mexico/slide-cursos-min.jpg'},
+            { src: 'http://www.italika.mx/WebVisorArchivosITK/Archivo.aspx?Tipo=3&Archivo=WebPortalMexicoITK/img/Slide/Mexico/slide-Modena.jpg'},
+            { src: 'http://www.italika.mx/WebVisorArchivosITK/Archivo.aspx?Tipo=3&Archivo=WebPortalMexicoITK/img/Slide/Mexico/refacciones.jpg'},
+            { src: 'http://www.italika.mx/WebVisorArchivosITK/Archivo.aspx?Tipo=3&Archivo=WebPortalMexicoITK/img/Slide/Mexico/slide-Fiera.jpg'},
+            { src: 'http://www.italika.mx/WebVisorArchivosITK/Archivo.aspx?Tipo=3&Archivo=WebPortalMexicoITK/img/Slide/Mexico/Beneficios.jpg'},
+        ],
+      }),
   }
 </script>
