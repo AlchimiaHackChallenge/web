7f05e27aa5f52481ed6529a363e180301992038e
Juancho <linanjm90@gmail.com>
2019-08-24 19:38:20 -0500

    Add a sub route view in the Main component

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,11 +1,16 @@
 <template>
   <v-app-bar app
              color="bar">
-      <v-toolbar-title
-        class="headline text-uppercase">
-        <span class="primary--text">Italika</span>
-      </v-toolbar-title>
-    </v-app-bar>
+    <v-toolbar-title
+      class="headline text-uppercase">
+      <span class="primary--text">Italika</span>
+    </v-toolbar-title>
+    <v-content>
+      <v-container fluid fill-height>
+        <router-view/>
+      </v-container>
+    </v-content>
+  </v-app-bar>
 </template>
 
 <script>
